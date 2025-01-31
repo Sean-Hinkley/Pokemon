b1ba09a6708da0b176b205cfc4cb80e27ff6923b
Hinkley <Se.G.Hinkley@baypath.net>
2025-01-31 11:24:09 -0500

    Basic Files

---
+++ Node/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+const connectionString = "mongodb+srv://sean:hi6yIe40@school.9eciq.mongodb.net/?retryWrites=true&w=majority&appName=School";
+
+MongoClient.connect(connectionString, {autoSelectFamily : false}).then(client => {
+    console.log('Connected to DB');
+    const db = client.db('company');
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(express.static('public'));
+
+    app.get('/', (req, res) => {
+        res.render('index.ejs');
+    })
+
+    
+
+    const port = 3000;
+    app.listen(port, function() {
+        console.log(`server on http://localhost:${port}`)
+    })
+}).catch(error => console.error(error))
+
